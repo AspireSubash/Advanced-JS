ed413145f69307bb4a7b6ccb305fe9b48a16335c
subash.muthusamy <subash.muthusamy@aspiresys.com>
2024-03-23 08:31:27 +0530

    demo user task

--- UserNameandPassword/script.js
+++ UserNameandPassword/script.js
@@ -2,11 +2,12 @@ function validate() {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
   var age = document.getElementById('age').value;
-  if (username.trim() == '' || password.trim() == '' || age.trim() == '') {
+  var mobile = document.getElementById('mobile').value;
+  if (username.trim() == '' || password.trim() == '' || age.trim() == '' || mobile.trim() == '') {
     alert('Please fill all the fields');
     console.log('Please fill all the fields');
     return false;
-  } else if (username == 'abc' && password == 'abc' && age >= '20') {
+  } else if (username == 'abc' && password == 'abc' && age >= '20' && mobile.length == '10') {
     console.log('Valid User');
     return true;
   } else {
